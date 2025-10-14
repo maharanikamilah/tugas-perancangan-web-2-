2e17f2fa367f0a26b780117483d52e4a9c56f1e1
maharanikamilah <hildagxng@gmail.com>
2025-10-14 10:33:16 +0700

    Add files via upload

--- P2/metodepost.php
+++ P2/metodepost.php
@@ -4,9 +4,9 @@
 </head> 
 <body> 
 <h1>Input</h1> 
-<form action="methodgetproses.php" method="get"> 
-Masukkan nama : <input type="text" name="nama" size="25"> 
+<form action="methodpostproses.php" method="post"> 
+Masukkan nama : <input type"text" name="nama" size="25"> 
 <input type="submit" value="Proses"> 
 </form> 
-</body> 
+</body> 
 </html>
